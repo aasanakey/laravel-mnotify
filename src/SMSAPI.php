8d72c88235b3a06bf93560f60af4b3da44ec6d79
Sanakey Augustine Ayiku <sanakeyaugustineayiku@gmail.com>
2020-12-28 18:32:09 +0000

    fixed syntax error in SMSAPI

--- src/SMSAPI.php
+++ src/SMSAPI.php
@@ -49,7 +49,7 @@ class SMSAPI {
         }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1,);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         $result = curl_exec($ch);
         curl_close ($ch);
